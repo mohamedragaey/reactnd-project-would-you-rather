5352d087dd65d84598310a15ff72d4678d8b1c53
ragaey <mohamedragaey33@gmail.com>
2019-06-30 01:47:33 +0200

    initial commit.

---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+import '../css/navbar.css'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+class Navbar extends Component {
+  render() {
+    const {user, authedUser} = this.props
+    return (
+      <div className="nav-container">
+        <div className="inner-container">
+          <div className="nav-brand">Would You Rather</div>
+          {authedUser ? (
+            <ul className="nav-list">
+              <li className="nav-item">
+                <Link to="/home">Home</Link>
+              </li>
+              <li className="nav-item purple">
+                <Link to="/add">Add question</Link>
+              </li>
+              <li className="nav-item split">
+                <Link to="/leaderboard">Leaderboard</Link>
+              </li>
+              <li className="nav-item">{user ? user.name : null}</li>
+              <li className="nav-item log-out">
+                <Link to="/">
+                  <button className="log-out-button">Log Out</button>
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav-list">
+              <li className="nav-item">Log in to ask a question</li>
+            </ul>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  const {users, authedUser} = state
+  const user = users[authedUser]
+  return {
+    user,
+    authedUser
+  }
+}
+
+export default connect(mapStateToProps)(Navbar)
